334bfcbda4258b64ddfcd80e5c145d7a650ad807
vitorfreitas <vitorfreitas@college.harvard.edu>
2015-12-04 05:24:00 +0000

    Adding buy.php

---
+++ public/buy.php
@@ -0,0 +1,11 @@
+<?php
+$uid = $_GET["user_id"];
+$pid = $_GET["product_id"];
+
+// vai no banco de dados, verifica se existe o produto e se QTD > 0
+
+// Cobra do usuario usando Stripe (VITAO)
+
+// Cria uma compra pro usuario
+
+?>
