c43258d32882b475a1d4f809ea9e6db621f86333
Renan Ferreirinha Carneiro <rcarneiro@college.harvard.edu>
2015-12-04 08:00:05 +0000

    updtes

--- public/index.php
+++ public/index.php
@@ -2,7 +2,15 @@
     // configuration
     require("../includes/config.php"); 
     
-    $products = [];// vem do mysql
+    /* Database query for the products */ 
+    $CS50.query == "SELECT * FROM products";
+    $products = mysql_query($query);
     
+    // Check if products exist 
+    if (!$products) 
+    {
+        die ("Query error $query: " . mysql_error());
+    }
+
     render("show_products.php", ["name" => "Renan", "products" => $products]);
 ?>
