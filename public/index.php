987cd87ce408f34d77b20448f51effbe82f1ad55
Renan Ferreirinha Carneiro <rcarneiro@college.harvard.edu>
2015-12-04 08:17:40 +0000

    Ignoring config

--- public/index.php
+++ public/index.php
@@ -4,12 +4,6 @@
     
     /* Database query for the products */ 
     $products = CS50::query("SELECT * FROM products");
-    
-    // Check if products exist 
-    if (!$products) 
-    {
-        die ("Query error $query: " . mysql_error());
-    }
 
     render("show_products.php", ["name" => "Renan", "products" => $products]);
 ?>
