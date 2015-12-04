8362ecf8b5dd8fca6547aa4fe8fbe55cb664d440
Matheus Cardelli De Souza Campos <matheuscampos@college.harvard.edu>
2015-12-04 08:18:14 +0000

    3rd commit

--- public/buy.php
+++ public/buy.php
@@ -8,22 +8,47 @@
 
 // Cria uma compra pro usuario rola
     $product = CS50 :: query ("SELECT * FROM T_PRODUTO WHERE id = ?", $pid);
+    $rows = CS50::query("SELECT * FROM T_PRODUTO WHERE id = ?", $pid);
     
-    if (count($product) == 0)
+    if (count($rows) == 0)
     {
-        apologize("Could not find the product")
+        apologize("Could not find the product");
     }
     
-    $checar_estoque = CS50 :: query ("SELECT estoque FROM T_PRODUTO where id = ?", $pid)
+    $check_rows = $rows["estoque"]
     
-    if ($checar_estoque <= 0)
+    if($check_rows <= 0)
     {
-        apologize ("There is no sufficient quantity")
+        apologize("There is no sufficient quantity");
     }
     
     // Cobra do usuario usando Stripe (VITAO)
+    
+      require_once('./config.php');
+    
+      $token  = $_POST['stripeToken'];
+    
+      $customer = \Stripe\Customer::create(array(
+          'email' => 'customer@example.com',
+          'card'  => $token
+      ));
+    
+      $charge = \Stripe\Charge::create(array(
+          'customer' => $customer->id,
+          'amount'   => 5000,
+          'currency' => 'usd'
+      ));
+    
+      echo '<h1>Successfully charged $50.00!</h1>';
+    
 
     // Cria uma compra pro usuario
+<<<<<<< HEAD
     CS50 :: query("UPDATE T_PRODUTO SET estoque = estoque - 1 WHERE id = ?", $pid);
     CS50 :: query ("INSERT INTO T_COMPRAS(ID_USUARIO, ID_PRODUTO) VALUES(?,?)" $pid, $uid);
 ?>
+=======
+    CS50::query("UPDATE T_PRODUTO SET estoque = estoque - 1 WHERE id = ?", $pid);
+    CS50::query("INSERT INTO T_COMPRAS(ID_USUARIO, ID_PRODUTO) VALUES(?,?)", $uid, $pid);
+?>
+>>>>>>> 3rd commit
